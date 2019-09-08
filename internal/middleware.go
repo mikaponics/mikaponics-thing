ddc43dd949b8104c63a3fb253c148f4cd12dca6c
bartmika <bart@mikasoftware.com>
2019-09-08 00:35:57 -0400

    BUGFIX

--- internal/middleware.go
+++ internal/middleware.go
@@ -41,7 +41,7 @@ func unaryInterceptor(
 	ctx context.Context,
 	req interface{},
 	info *grpc.UnaryServerInfo,
-	handler grpc.UnaryHandler
+	handler grpc.UnaryHandler,
 ) (interface{}, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 
