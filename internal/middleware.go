e3074c4b8ed8788badecf77fad319a5bbaa658f2
bartmika <bart@mikasoftware.com>
2019-09-08 00:28:49 -0400

    Minor formatting

--- internal/middleware.go
+++ internal/middleware.go
@@ -1,6 +1,7 @@
 /**
  *  THIS IS A SAMPLE FILE WHICH WORKS AND IS FOR TESTING PURPOSES.
  *  - https://github.com/kelseyhightower/grpc-hello-service
+ *  - https://davidsbond.github.io/2019/06/14/creating-grpc-interceptors-in-go.html
  *
  */
 
@@ -36,7 +37,12 @@ func valid(authorization []string) bool {
 }
 
 
-func unaryInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+func unaryInterceptor(
+	ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler
+) (interface{}, error) {
 	md, ok := metadata.FromIncomingContext(ctx)
 
 	if !ok {
